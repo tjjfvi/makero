ba201540864b98b38424bc48db263282c7e625f7
tjjfvi <tjjfvi@tjjfvi.com>
2021-09-10 20:01:07 -0700

    use braces in makero-generated macro recursive calls to support item macros

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,44 @@ macro_rules! _makero {
     macro_rules! $name {
       $($(
         (_makero (fn $fn $d _makero_return:tt) $($arg)*) => {
-          $name!(_makero $d _makero_return $($result)*)
+          $name!{_makero $d _makero_return $($result)*}
         };
         (_makero impl $d prev:tt ($fn ! ($d($d arg:tt)*) $d($d next:tt)*) $d ret:tt) => {
-          $name!(_makero impl () ($d($d arg)*) (fn $fn (impl $d prev ($d($d next)*) $d ret)))
+          $name!{_makero impl () ($d($d arg)*) (fn $fn (impl $d prev ($d($d next)*) $d ret))}
         };
       )*)*
       (_makero impl $d prev:tt (($d($d inner:tt)*) $d($d next:tt)*) $d ret:tt) => {
-        $name!(_makero impl () ($d($d inner)*) (impl wrap () $d prev ($d($d next)*) $d ret))
+        $name!{_makero impl () ($d($d inner)*) (impl wrap () $d prev ($d($d next)*) $d ret)}
       };
       (_makero impl $d prev:tt ([$d($d inner:tt)*] $d($d next:tt)*) $d ret:tt) => {
-        $name!(_makero impl () ($d($d inner)*) (impl wrap [] $d prev ($d($d next)*) $d ret))
+        $name!{_makero impl () ($d($d inner)*) (impl wrap [] $d prev ($d($d next)*) $d ret)}
       };
       (_makero impl $d prev:tt ({$d($d inner:tt)*} $d($d next:tt)*) $d ret:tt) => {
-        $name!(_makero impl () ($d($d inner)*) (impl wrap {} $d prev ($d($d next)*) $d ret))
+        $name!{_makero impl () ($d($d inner)*) (impl wrap {} $d prev ($d($d next)*) $d ret)}
       };
       (_makero impl ($d($d prev:tt)*) ($d cur:tt $d($d next:tt)*) $d ret:tt) => {
-        $name!(_makero impl ($d($d prev)* $d cur) ($d($d next)*) $d ret)
+        $name!{_makero impl ($d($d prev)* $d cur) ($d($d next)*) $d ret}
       };
       (_makero impl ($d($d prev:tt)*) () $d ret:tt) => {
-        $name!(_makero $d ret $d($d prev)*)
+        $name!{_makero $d ret $d($d prev)*}
       };
       (_makero (impl wrap () ($d($d prev:tt)*) ($d($d next:tt)*) $d ret:tt) $d($d cur:tt)*) => {
-        $name!(_makero impl ($d($d prev)* ($d($d cur)*)) ($d($d next)*) $d ret)
+        $name!{_makero impl ($d($d prev)* ($d($d cur)*)) ($d($d next)*) $d ret}
       };
       (_makero (impl wrap [] ($d($d prev:tt)*) ($d($d next:tt)*) $d ret:tt) $d($d cur:tt)*) => {
-        $name!(_makero impl ($d($d prev)* [$d($d cur)*]) ($d($d next)*) $d ret)
+        $name!{_makero impl ($d($d prev)* [$d($d cur)*]) ($d($d next)*) $d ret}
       };
       (_makero (impl wrap {} ($d($d prev:tt)*) ($d($d next:tt)*) $d ret:tt) $d($d cur:tt)*) => {
-        $name!(_makero impl ($d($d prev)* {$d($d cur)*}) ($d($d next)*) $d ret)
+        $name!{_makero impl ($d($d prev)* {$d($d cur)*}) ($d($d next)*) $d ret}
       };
       (_makero (impl $d prev:tt ($d($d next:tt)*) $d ret:tt) $d($d cur:tt)*) => {
-        $name!(_makero impl $d prev ($d($d cur)* $d($d next)*) $d ret)
+        $name!{_makero impl $d prev ($d($d cur)* $d($d next)*) $d ret}
       };
       (_makero return $d($d result:tt)*) => {
         $d($d result)*
       };
       ($d($d x:tt)*) => {
-        $name!(_makero impl () ($name!($d($d x)*)) return)
+        $name!{_makero impl () ($name!($d($d x)*)) return}
       };
     }
   };
